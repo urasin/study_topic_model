73a58ec1598d86ce7a31dacb903d7ccc03a7cf49
urasin <urasin2012@gmail.com>
2019-01-01 18:32:20 +0900

    ngram

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hogeeee")
+# https://qiita.com/shuntaroy/items/b0e557be3406a9b09ade
+def ngram(words, n):
+  return [tuple(words[i:i + n]) for i in range(len(words) - n + 1)]
+  
+
+# print(ngram("this is a pen", 1)[0][1])
+print(len(tuple([1])))
+
+# ngram コーパス参考
+# https://www.slideshare.net/moaikids/ngram-4770283
